058c7c7a45e4984223fd437a0db42de573c2b103
Rees Dart <thedominicparker@gmail.com>
2021-06-14 08:29:49 -0700

    connecting redux to simple DOMUI and reactUI

---
+++ reduxStore+reactUI.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
+// REDUX CODE
+///////////////////////////////////
+
+const increment = () => {
+  return {type: 'increment'} 
+}
+
+const decrement = () => { 
+  return {type: 'decrement'}
+}
+
+const initialState = 0;
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'increment':
+      return state + 1;
+    case 'decrement':
+      return state - 1;
+    default:
+      return state; 
+  }
+} 
+
+const store = createStore(counterReducer);
+
+// REACT CODE
+///////////////////////////////////
+
+const render = () => {
+  ReactDOM.render(
+    <CounterApp
+      state={store.getState()}
+    />,
+    document.getElementById('root')
+  )
+}
+
+
+// Render once with the initial state.
+render();
+// Subscribe render to changes to the store's state.
+
+function CounterApp(props) {
+  let state = props.state;
+  const onIncrementButtonClicked = () => {
+    store.dispatch(increment());
+  }
+ 
+  const onDecrementButtonClicked = () => {
+    store.dispatch(decrement());
+  }
+  
+  return (   
+    <div id='counter-app'>
+      <h1> {state} </h1>
+      <button onClick={onIncrementButtonClicked}>+</button> 
+      <button onClick={onDecrementButtonClicked}>-</button>
+    </div>
+  )
+}
+
+store.subscribe(render);
